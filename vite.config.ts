d62dc26528857c73b3a3dfd1af890724e11b87c1
Leo Salazar <leo.salazar@yum.com>
2025-11-24 11:00:36 -0800

    Remove unused parameter in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const base = process.env.GITHUB_PAGES === 'true' ? '/pcal/' : '/'
 
   return {
